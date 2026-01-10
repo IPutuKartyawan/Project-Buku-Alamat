a746bf3677e582dc440d5ea09f2aac5a1d44b000
Putu Kartyawan <iputukartyawan@gmail.com>
2026-01-10 20:38:07 +0800

    dashboard final ast

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,10 @@
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/check_session.php';
 require_once __DIR__ . '/config/database.php';
-
-
-$baseUrl =
-    (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http')
-    . '://' . $_SERVER['HTTP_HOST']
-    . dirname($_SERVER['SCRIPT_NAME']);
 ?>
 <!DOCTYPE html>
 <html lang="id">
@@ -17,17 +12,16 @@ $baseUrl =
     <meta charset="UTF-8">
     <title>Dashboard</title>
 
-
-    <link rel="stylesheet" href="<?= $baseUrl ?>/asset/style.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
 <div class="navbar">
     <div class="brand">Buku Alamat</div>
     <div class="nav-link">
-        <a href="<?= $baseUrl ?>/dashboard.php">Dashboard</a>
-        <a href="<?= $baseUrl ?>/contacts/index.php">Kontak</a>
-        <a href="<?= $baseUrl ?>/auth/logout.php">Logout</a>
+        <a href="dashboard.php">Dashboard</a>
+        <a href="contacts/index.php">Kontak</a>
+        <a href="auth/logout.php">Logout</a>
     </div>
 </div>
 
@@ -40,14 +34,14 @@ $baseUrl =
     </p>
 
     <p>
-        Melalui dashboard ini, pengguna dapat mengelola seluruh
+        Melalui halaman ini, pengguna dapat mengelola seluruh
         data kontak yang dimiliki, mulai dari menambah, mengubah,
         menghapus, hingga melihat detail informasi kontak secara terstruktur.
     </p>
 </div>
 
 <div class="footer">
-    &copy; <?= date('Y'); ?> Sistem Buku Alamat | PHP Native
+    &copy; <?= date('Y'); ?> Buku Alamat Kenalan | Simpan Kontak dan Informasi
 </div>
 
 </body>
