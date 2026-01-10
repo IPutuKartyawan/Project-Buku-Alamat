75e228b64e2c1623f11c03e8c2617346c026a958
Putu Kartyawan <iputukartyawan@gmail.com>
2026-01-10 20:50:50 +0800

    Query sql database

--- schema.sql
+++ schema.sql
@@ -20,3 +20,13 @@ CREATE TABLE contacts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+INSERT INTO contacts (user_id, name, phone, email, address, category)
+VALUES
+(1, 'Ibu Sari', '081234567890', 'sari@gmail.com', 'Jl. Merdeka No. 1', 'Keluarga'),
+(1, 'Pakyan KOster', '08256789055', 'goodbegood@gmail.com', 'Jl. Tengah Merdeka', 'Keluarga'),
+(1, 'Budi Santoso', '082345678901', 'budi@gmail.com', 'Jl. Sudirman No. 10', 'Teman'),
+(1, 'Rina Wijaya', '083456789012', 'rina@gmail.com', 'Jl. Diponegoro No. 5', 'Kerja'),
+(1, 'Ahmad Fauzi', '084567890123', 'ahmad@gmail.com', 'Jl. Ahmad Yani No. 7', 'Kerja'),
+(1, 'Dewi Lestari', '085678901234', 'dewi@gmail.com', 'Jl. Melati No. 3', 'Lainnya'),
+(1, 'Wayan Jenar', '0856784677382', 'yanjenaar@gmail.com', 'Jl. Gajahmada dangin puri', 'Teman');
